f9405387059a3a8a4e80a6ea57984dc2ca9c9757
ksmitul <mitul6282@gmail.com>
2024-08-11 00:32:41 -0400

    add users table

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ import pandas as pd
 import sqlite3
 import os
 import logging
+import tracemalloc
 from dotenv import load_dotenv
 load_dotenv()
 
+tracemalloc.start()
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s : %(message)s')
 logger= logging.getLogger()
@@ -18,23 +20,6 @@ checkin_path = 'data\yelp_academic_dataset_checkin.json'
 business_path = 'data\yelp_academic_dataset_business.json'
 review_path = 'data\yelp_academic_dataset_review.json'
 
-# Read the json file into pandas dataframe to load into the database. 
-
-logger.info('Read JSON data into Pandas Dataframe')
-df_tips = pd.read_json(tip_path, lines=True)
-df_tips = df_tips.rename(columns={
-    "user_id": "user_id",
-    "business_id": "business_id",
-    "text": "review_text",
-    "date": "review_date",
-    "compliment_count": "compliment_count"
-})
-
-db_path = os.getenv('db_path')
-
-# Create the database schema using sql migration script. 
-
-
 def apply_migration_script(db_path, script_path):
     with sqlite3.connect(db_path) as conn:
         with open(script_path, 'r') as f:
@@ -42,11 +27,30 @@ def apply_migration_script(db_path, script_path):
         conn.executescript(script)
         conn.commit()
 
+db_path = os.getenv('db_path')
 script_path = 'db_migrations\create_table._0001up.sql'
 logger.info('Creating the Database schema')
 apply_migration_script(db_path, script_path)
 
+# Read the json file into pandas dataframe to load into the database. 
+
+logger.info('Read JSON data into Pandas Dataframe')
+df = pd.read_json(tip_path, lines=True, engine= 'pyarrow')
+df = df.rename(columns={
+    "user_id": "user_id",
+    "business_id": "business_id",
+    "text": "review_text",
+    "date": "review_date",
+    "compliment_count": "compliment_count"
+})
+
+# Create the database schema using sql migration script. 
+
 conn = sqlite3.connect(db_path)
 logger.info('Loading the data into DB')
-df_tips.to_sql('tips', con=conn, if_exists='append', index=False)
-logger.info('Data insert successful')
+df.to_sql('tips', con=conn, if_exists='append', index=False)
+for chunk in pd.read_json(user_path, lines=True, chunksize=10000):
+    chunk.to_sql('users', con=conn, if_exists='append', index=False)
+
+current, peak = tracemalloc.get_traced_memory()
+logger.info(f'Data insert successful. Current memory usage: {round(current / 10**6, 2)}MB; Peak was: {round(peak / 10**6, 2)}MB')
